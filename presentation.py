61ee27b9a7dfcc12cacc051d1fa2b0bd0ee8389c
Gabriel Desharnais <gabriel.desharnais@hotmail.com>
2016-12-07 12:39:27 -0500

    Le module qui fait le travail

---
+++ presentation.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+# coding: utf8
+from matplotlib import rcParams
+params = {'axes.linewidth': 1.0,
+    'axes.labelsize': 30,
+    'font.size': 30,
+    'legend.borderaxespad': 0.1,
+    'legend.borderpad': 0.1,
+    'legend.columnspacing': 0.6,
+    'legend.fontsize': 25,
+    'legend.handlelength': 0.5,
+    'legend.handletextpad': 0.25,
+    'legend.labelspacing': 0.1,
+    'legend.numpoints': 1,
+    'lines.linewidth': 2.0,
+    'lines.markersize': 10,
+    'xtick.labelsize': 30,
+    'ytick.labelsize': 30,
+    'axes.unicode_minus': False,
+    'font.family': 'sans-serif',
+    'xtick.major.pad': 4,
+    'xtick.major.size': 4,
+    'xtick.minor.pad': 4,
+    'xtick.minor.size': 2,    
+    'ytick.major.pad': 4,
+    'ytick.major.size': 4,
+    'ytick.minor.pad': 4,
+    'ytick.minor.size': 2}
+rcParams.update(params)
